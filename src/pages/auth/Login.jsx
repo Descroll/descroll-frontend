f05a443371b7a2511387659621528786812d15b3
batul0 <batul.anous@gmail.com>
2026-03-16 21:27:54 -0400

    basic signup/login pages without css styling

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,9 +1,21 @@
 function Login() {
     return (
         <div>
-            
+            <h1>Descroll </h1>
+            <h2>Welcome Back! </h2>
+
+            <input type="email" placeholder="Email address" />
+            <input type="password" placeholder="Password" />
+
+            <button>Login</button>
+
+            <p>
+                Dont have an account yet? Sign Up
+            </p>
+
         </div>
 
     
     );
 }
+export default Login;
--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -1,8 +1,26 @@
 function Signup() {
     return (
         <div>
-            
+            <h1>DeScroll </h1>
+            <h2>Get Started Now</h2>
+
+            <input type = "text" placeholder = "Username" />
+            <input type = "text" placeholder = "Email" />
+            <input type = "text" placeholder = "Password" />
+
+            <label>
+                <input type = "checkbox" />
+                I agree to the terms & policy
+            </label>
+
+            <button> Signup </button>
+
+            <p>
+                Have an account already? Log in
+            </p>
+
         </div>
 
     );
 }
+export default Signup; 
